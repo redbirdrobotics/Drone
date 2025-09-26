62164b3c5ed782cb5f0d48e7e562935cb3bfb8b9
Austin <austinboone305@gmail.com>
2025-09-25 21:56:51 -0400

    doing a kattis problem

--- test.cpp
+++ test.cpp
@@ -1,8 +1,37 @@
+//this doesnt work I was just messing around with a kattis problem
+
 #include <iostream>
+
 using namespace std;
+
 int main(){
-
-	cout << "hello world!";
-
-	return 0;
+    string word = "";
+    cin >> word;
+    bool oneOp = false;
+    bool zeroOp = false;
+    bool twoOp = false;
+    for (int i = 0; i < word.length(); i++){
+        if (word.substr(i, i + 1).compare("l") == 0 or word.substr(i, i + 1).compare("v") == 0){
+            oneOp = true;
+        }
+        else if (word.substr(i, i + 2).compare("lv") == 0){
+            zeroOp = true;
+            break;
+        }
+        else if (i == word.length()){
+            twoOp = true;
+        }
+    }
+    if (zeroOp){
+        cout << 0;
+        return 0;
+    }
+    else if (oneOp){
+        cout << 1;
+        return 0;
+    }
+    else{
+        cout << 2;
+        return 0;
+    }
 }
