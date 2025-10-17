0cd75ccebffd28d3205df483118a5ccff3e806a6
Austin <austinboone305@gmail.com>
2025-10-16 22:20:33 -0400

    added 3d files

--- test.cpp
+++ test.cpp
@@ -1,37 +1,30 @@
-//this doesnt work I was just messing around with a kattis problem
-
 #include <iostream>
-
 using namespace std;
 
-int main(){
+int main() {
     string word = "";
     cin >> word;
     bool oneOp = false;
     bool zeroOp = false;
-    bool twoOp = false;
-    for (int i = 0; i < word.length(); i++){
-        if (word.substr(i, i + 1).compare("l") == 0 or word.substr(i, i + 1).compare("v") == 0){
-            oneOp = true;
-        }
-        else if (word.substr(i, i + 2).compare("lv") == 0){
+
+    for (int i = 0; i < word.length(); i++) {
+        if (word.substr(i, 2) == "lv") {  // check two-character substring
             zeroOp = true;
             break;
         }
-        else if (i == word.length()){
-            twoOp = true;
+        else if (word.substr(i, 1) == "l" || word.substr(i, 1) == "v") {
+            oneOp = true;
         }
     }
-    if (zeroOp){
+
+    if (zeroOp) {
         cout << 0;
-        return 0;
     }
-    else if (oneOp){
+    else if (oneOp) {
         cout << 1;
-        return 0;
     }
-    else{
+    else {
         cout << 2;
-        return 0;
     }
+    return 0;
 }
